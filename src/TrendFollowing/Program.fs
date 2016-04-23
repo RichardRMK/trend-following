3443ed9d7493a41dc7efbe81b7891c7b4d93a7e5
Jim Killingsworth <jkillingsworth@gmail.com>
2016-04-22 17:35:52 -0700

    Use stopwatch to measure simulation run time

--- src/TrendFollowing/Program.fs
+++ src/TrendFollowing/Program.fs
@@ -1,7 +1,13 @@
 ﻿module Program
 
+open System.Diagnostics
 open TrendFollowing
 
 //-------------------------------------------------------------------------------------------------
 
+let stopwatch = Stopwatch.StartNew()
+
 Processing.runSimulation Experiment01.simulation
+
+stopwatch.Stop()
+printfn "%A" stopwatch.Elapsed
