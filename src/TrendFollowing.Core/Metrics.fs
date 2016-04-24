cb2061a1fc717dacf009ea73c890edcdae94783a
Jim Killingsworth <jkillingsworth@gmail.com>
2016-04-23 22:18:32 -0700

    Improve performance computing adjusted amount

--- src/TrendFollowing.Core/Metrics.fs
+++ src/TrendFollowing.Core/Metrics.fs
@@ -9,4 +9,6 @@ let computeAdjustedAmount (recordsLog : RecordsLog) (prevElementLog : ElementLog
     let splitOld = recordsLog.SplitOld
     let dividend = recordsLog.Dividend
     let basis = prevElementLog.RecordsLog.Close
-    Processing.computeAdjustedAmount splitNew splitOld dividend basis
+    match (splitNew, splitOld, dividend) with
+    | (1u, 1u, 0m) -> id
+    | _ -> Processing.computeAdjustedAmount splitNew splitOld dividend basis
