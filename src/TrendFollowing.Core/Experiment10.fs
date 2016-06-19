152137b966e40b96b8f194053948ee3c65ade253
Jim Killingsworth <jkillingsworth@gmail.com>
2016-06-19 14:53:25 -0700

    Add experiments that compute position size to maximize leverage

---
+++ src/TrendFollowing.Core/Experiment10.fs
@@ -0,0 +1,113 @@
+﻿module TrendFollowing.Experiment10
+
+open System
+open TrendFollowing.Types
+open TrendFollowing.Metrics
+open TrendFollowing.Output
+
+//-------------------------------------------------------------------------------------------------
+
+let private paramRisk = 1.0m
+let private paramWait = 200u
+let private paramRes = 200
+let private paramSup = 200
+
+//-------------------------------------------------------------------------------------------------
+
+type MetricsLog =
+    { ResLookback    : decimal[]
+      SupLookback    : decimal[]
+      Res            : decimal
+      Sup            : decimal
+      TrendDirection : TrendDirection }
+
+let private computeMetricsLogInit (recordsLog : RecordsLog) =
+
+    { ResLookback    = Array.create paramRes recordsLog.Hi
+      SupLookback    = Array.create paramSup recordsLog.Lo
+      Res            = recordsLog.Hi
+      Sup            = recordsLog.Lo
+      TrendDirection = Negative }
+
+let private computeMetricsLogNext (recordsLog : RecordsLog) (prevElementLog : ElementLog<MetricsLog>) =
+
+    let computeAdjustedAmount =
+        Metrics.computeAdjustedAmount recordsLog prevElementLog
+
+    let resLookback =
+        prevElementLog.MetricsLog.ResLookback
+        |> Array.map computeAdjustedAmount
+        |> Array.append [| recordsLog.Hi |]
+        |> Array.take paramRes
+
+    let supLookback =
+        prevElementLog.MetricsLog.SupLookback
+        |> Array.map computeAdjustedAmount
+        |> Array.append [| recordsLog.Lo |]
+        |> Array.take paramSup
+
+    let res = resLookback |> Array.max
+    let sup = supLookback |> Array.min
+
+    let trendDirection =
+        match prevElementLog.MetricsLog with
+        | prevMetricsLog when recordsLog.Hi >= computeAdjustedAmount prevMetricsLog.Res -> Positive
+        | prevMetricsLog when recordsLog.Lo <= computeAdjustedAmount prevMetricsLog.Sup -> Negative
+        | prevMetricsLog -> prevMetricsLog.TrendDirection
+
+    { ResLookback    = resLookback
+      SupLookback    = supLookback
+      Res            = res
+      Sup            = sup
+      TrendDirection = trendDirection }
+
+let computeMetricsLog (recordsLog : RecordsLog) = function
+    | None      -> computeMetricsLogInit recordsLog
+    | Some prev -> computeMetricsLogNext recordsLog prev
+
+//-------------------------------------------------------------------------------------------------
+
+let computeTakeOrders (elementLogs : ElementLog<MetricsLog>[]) (summaryLog : SummaryLog) : TakeOrder[] =
+
+    let computeOrder elementLog : TakeOrder =
+
+        let res = elementLog.MetricsLog.Res
+        let cash = summaryLog.Cash
+        let shares = (cash * paramRisk) / res
+
+        { Ticker = elementLog.RecordsLog.Ticker
+          Shares = uint32 shares }
+
+    elementLogs
+    |> Array.filter (fun x -> x.RecordsLog.Count >= paramWait)
+    |> Array.filter (fun x -> x.RecordsLog.Shares = 0u)
+    |> Array.filter (fun x -> x.MetricsLog.TrendDirection = Positive)
+    |> Array.map computeOrder
+
+let calculateExitStop (elementLog : ElementLog<MetricsLog>) : decimal =
+
+    elementLog.MetricsLog.Sup
+
+//-------------------------------------------------------------------------------------------------
+
+let reportAgent = ReportAgent()
+
+let dateStart = DateTime(1990, 01, 01)
+let dateFinal = DateTime.Today
+let getQuotes = Data.getQuotes dateStart dateFinal
+let dates = Data.getDates dateStart dateFinal
+
+let model =
+    { ComputeMetricsLog = computeMetricsLog
+      ComputeTakeOrders = computeTakeOrders
+      CalculateExitStop = calculateExitStop }
+
+let simulation =
+    { Principal         = 1000000.00m
+      GetQuotes         = getQuotes
+      Dates             = dates
+      Model             = model
+      ReportElementLog  = reportAgent.ReportElementLog
+      ReportSummaryLog  = reportAgent.ReportSummaryLog
+      ReportJournalLog  = reportAgent.ReportJournalLog
+      ReportCompletion  = reportAgent.ReportCompletion }
